0fe71aa4c14acc9dc7159c1b444a28abac121440
Alex Mitchell <alexj.mitchell94@gmail.com>
2019-12-02 13:19:20 -0800

    using a prebuilt react component ingredients now display on an accordian

--- client/src/components/recipe/ingredient-list.js
+++ client/src/components/recipe/ingredient-list.js
@@ -1,19 +1,28 @@
 import React from "react"
+import Collapsible from "react-collapsible"
 import { useIngredientsList } from "../../hooks"
+import '../../styles/recipe/ingredient-list.css'
 
 const IngredientList = props => {
   const { ingredients, food_group } = useIngredientsList()
-  console.log(food_group)
+
   return (
-    <div>
-      {food_group.map(group => (
+    <div className="ingredient-list-container">
+      {/* {food_group.map(group => (
         <dl>
           <dt>{group}</dt>
           {ingredients.map(ingredient => (
-            <dd>{ingredient.food_group === group ? ingredient.name : ''}</dd>
+            <dd>{ingredient.food_group === group ? ingredient.name : ""}</dd>
           ))}
         </dl>
-      ))}
+      ))} */}
+     {food_group.map(group => (
+       <Collapsible trigger={group}>
+         {ingredients.map(ingredient => (
+            <p>{ingredient.food_group === group ? ingredient.name : ""}</p>
+          ))}
+       </Collapsible>
+     ))}
     </div>
   )
 }
