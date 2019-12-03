d31d133923990f3c3a6b94a8ebe4a4532aeb7e30
Luis Alonso <if.create702@gmail.com>
2019-12-02 17:47:43 -0800

    worked on master

--- client/src/components/login/toggle.js
+++ client/src/components/login/toggle.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react'
-import CreateSignUp from './signUpForm'
-import SignIn from './signInForm'
+import React, { useState } from "react"
+import CreateSignUp from "./signUpForm"
+import SignIn from "./signInForm"
 
-export default function Toggle(props){
-const [ userSignedup, setUserSignedUp] = useState(false)
-const [ log, setLog ] = useState(false)
+export default function Toggle(props) {
+  const [userSignedup, setUserSignedUp] = useState(false)
+  const [log, setLog] = useState(false)
 
-function handleToggle(){
-  setUserSignedUp(!userSignedup)
-  setLog(!log)
-}
-    return(
-        <div>
-            <button onClick={handleToggle}>{!log ? 'Register' : 'Login'}</button>
-            { userSignedup ? <CreateSignUp /> : <SignIn/> }
-        </div>
-    )
+  function handleToggle() {
+    setUserSignedUp(!userSignedup)
+    setLog(!log)
+  }
+  return (
+    <div>
+      <button onClick={handleToggle}>{!log ? "Register" : "Login"}</button>
+      {!userSignedup ? <CreateSignUp /> : <SignIn />}
+    </div>
+  )
 }
