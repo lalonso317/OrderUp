01f8776dc91162bafc1b6bd3c671f28c3a7af136
Mitchell Rhoads <mitchelldrhoads@gmail.com>
2019-12-04 17:28:01 -0800

    created user profile page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,18 +8,21 @@ import "semantic-ui-css/semantic.min.css"
 import PublicHomePage from "./home/index"
 import RecipeDirections from "./recipe/createRecipe-Directions"
 import CreateRecipeViewPage from "./recipe/create-recipe-viewpage"
+import UserProfileMain from "./profile/profilePage"
 import "../styles/recipe/ings_and_meas.css"
 import "../styles/recipe/index.css"
 import "../styles/recipe/ingredient-list.css"
 import "../styles/recipe/createRecipeHeader.css"
 import "../styles/recipe/createRecipeDirections.css"
 import "../styles/recipe/createRecipeSubmitButton.css"
+import "../styles/profile/profilePage.css"
 const App = props => {
   return (
     <Router>
       <Route exact path={"/"} component={PublicHomePage} />
       <Route path="/login" component={Toggle} />
       <Route path="/create-your-recipe" component={CreateRecipeViewPage} />
+      <Route path={"/profilePage"} component={UserProfileMain} />
       {/* <div className="ingsList-IandM">
         <IngredientList />
         <IngsAndMeas />
