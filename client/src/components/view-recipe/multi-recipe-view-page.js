2552efc835bb03609346b2105f94d8bfba93a720
Alex Mitchell <alexj.mitchell94@gmail.com>
2019-12-05 17:03:07 -0800

    made header and footer persist across all pages

--- client/src/components/view-recipe/multi-recipe-view-page.js
+++ client/src/components/view-recipe/multi-recipe-view-page.js
@@ -1,16 +1,9 @@
 import React from "react"
-import Header from "../home/Header"
 import "../../styles/recipe/multi-recipe-view-page.css"
 
 const MultiRecipeViewPage = props => {
   return (
     <div className="multi-recipe-view-page-container">
-      <Header
-        link_twitter="https://twitter.com/?lang=en"
-        link_facebook="https://www.facebook.com/"
-        link_reddit="https://www.reddit.com/"
-        link_instagram="https://www.instagram.com/"
-      />
       <h1 className="multi-recipe-view-page-title">Recipes</h1>
       <hr />
       <dl className="multi-recipe-view-page-details">
